dc5f7c2670d8b06ec705de1e50855a89827f0889
Sam Leeflang <sam.leeflang@naturalis.nl>
2025-12-03 09:23:44 +0100

    Fix properties

--- src/main/java/eu/dissco/dataexporter/properties/SecurityProperties.java
+++ src/main/java/eu/dissco/dataexporter/properties/SecurityProperties.java
@@ -3,9 +3,10 @@ package eu.dissco.dataexporter.properties;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.validation.annotation.Validated;
 
 @Data
-@NotBlank
+@Validated
 @ConfigurationProperties("security")
 public class SecurityProperties {
 
